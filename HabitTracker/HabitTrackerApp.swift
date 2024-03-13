39519889bcec74c3b63e3b20e987ad7f49cd219b
marissaalmoslino <marissaalmoslino@gmail.com>
2024-03-13 10:58:51 -0400

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Marissa Almoslino on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
